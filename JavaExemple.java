29a59f0e6a535723cda558c68e92c780bd265b1f
REYNIER <66683621+eric-seedle@users.noreply.github.com>
2020-08-10 17:15:43 +0200

    Update JavaExemple.java

--- JavaExemple.java
+++ JavaExemple.java
@@ -3,9 +3,14 @@ public class JavaExemple{
   public static void main(String[] args){
     System.out.println("Test");
     System.out.println(getValue());
+    System.out.println(getValue("Eric"));
   }
   
   public static String getValue(){
     return "test";
+  }
+  
+  public static String getValue(String name){
+    return name + " test";
   }
 }
