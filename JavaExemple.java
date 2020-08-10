164e99340de93dddfe9e15a6cc57c06ad05f0ac0
REYNIER <66683621+eric-seedle@users.noreply.github.com>
2020-08-10 16:43:23 +0200

    Update JavaExemple.java

--- JavaExemple.java
+++ JavaExemple.java
@@ -2,6 +2,7 @@ public class JavaExemple{
 
   public static void main(String[] args){
     System.out.println("Test");
+    System.out.println(getValue());
   }
   
   public static String getValue(){
